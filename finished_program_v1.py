859f5edb7890f078fe9f3c2542411f0fa70bb1f7
twschool <98859425+twschool@users.noreply.github.com>
2022-05-18 14:16:57 +1200

    Finished formatting code
    Removed all debug features as well as changed all # code comments longer than one line into paragraph comments like """comment1
    comment2"""
    Finished formatting code

--- finished_program_v1.py
+++ finished_program_v1.py
@@ -51,6 +51,7 @@ def options_checker(options_input):
         return False
 
 
+# Generate options function also outputs all wrong answers
 def generate_options(number_question_data, full_list_constant):
     options = ["A", "B", "C", "D"]
 
@@ -75,31 +76,31 @@ def generate_options(number_question_data, full_list_constant):
     # repeat 3 times (only 3 times because the correct
     # option has already been added
     for count in range(0, 3):
-        # Chooses a random option from the temporary
-        # list 1 which is the same as number_questions_stored
-        # except the correct answer is removed and
-        # the options that are picked are removed from the list
-        # to avoid duplicate answers
+        """Chooses a random option from the temporary
+        list 1 which is the same as number_questions_stored
+        except the correct answer is removed and
+        the options that are picked are removed from the list
+        to avoid duplicate answers"""
         option_picked = random.choice(temporary_list_1)
         temporary_list_1.remove(option_picked)
-        # Temporary_list_2 is used to store all the 3 random
-        # options as well as the 1 correct answer
-        # It picks the first thing in the list as it
-        # needs the name and not the correct number
+        """Temporary_list_2 is used to store all the 3 random
+        options as well as the 1 correct answer
+        It picks the first thing in the list as it
+        needs the name and not the correct number"""
         temporary_list_2.append(option_picked[0])
     for count in range(0, 4):
-        # All the random answers are in a list
-        # in temporary_options_2 like this
-        # ["Actual answer","Random", "Random", "Random"]
-        # since the correct answer is first in the list I created
-        # another list called random_options to resort everything into random
-        # order, so it would be easier to put them into options
+        """All the random answers are in a list
+        in temporary_options_2 like this
+        ["Actual answer","Random", "Random", "Random"]
+        since the correct answer is first in the list I created
+        another list called random_options to resort everything into random
+        order, so it would be easier to put them into options"""
         selected = random.choice(temporary_list_2)
         random_options.append(selected)
         temporary_list_2.remove(selected)
-        # Random_options is a list of random options
-        # and the correct answer in a random order
-        # This makes it easy to print it out randomly
+        """Random_options is a list of random options
+        and the correct answer in a random order
+        This makes it easy to print it out randomly"""
     while True:
         print(f"What is {correct_picked_answer} in Māori? ")
         print(f'(A) {random_options[0]} \t (B) {random_options[1]} \n'
@@ -120,30 +121,36 @@ def generate_options(number_question_data, full_list_constant):
 
 
 # Main routine
-# The variable which the questions (number) that have been asked are taken out of
+"""The variable which the questions (number)
+that have been asked are taken out of"""
 number_questions_stored_main = [["Tahi", 1], ["Rua", 2],
                                 ["Toru", 3], ["Whā", 4],
                                 ["Rima", 5], ["Ono", 6],
                                 ["Whitu", 7], ["Waru", 8],
                                 ["Iwa", 9], ["Tekau", 10]]
-# The variable which the questions (weekdays) that have been asked are taken out of
+
+"""The variable which the questions (weekdays)
+that have been asked are taken out of"""
 days_of_the_week_stored_main = [["Rāhina", "Monday"], ["Rātū", "Tuesday"],
                                 ["Rāapa", "Wednesday"],
                                 ["Rāpare", "Thursday"], ["Rāmere", "Friday"],
                                 ["Rāhoroi", "Saturday"], ["Rātapu", "Sunday"]]
-# This variable stores all the number
-# answers and is used for generating random options
+
+"""This variable stores all the number
+answers and is used for generating random options"""
 number_questions_constant = [["Tahi", 1], ["Rua", 2],
                              ["Toru", 3], ["Whā", 4], ["Rima", 5],
                              ["Ono", 6], ["Whitu", 7],
                              ["Waru", 8], ["Iwa", 9], ["Tekau", 10]]
-# This variable stores all the weekday
-# answers and is used for generating random options
+
+"""This variable stores all the weekday
+answers and is used for generating random options"""
 days_of_the_week_constant = [["Rāhina", "Monday"],
                              ["Rātū", "Tuesday"], ["Rāapa", "Wednesday"],
                              ["Rāpare", "Thursday"], ["Rāmere", "Friday"],
                              ["Rāhoroi", "Saturday"], ["Rātapu", "Sunday"]]
 
+# This variable defines the starting number of rounds
 round_number = 1
 
 wrong_questions = []
@@ -155,36 +162,35 @@ do_show_instructions = yes_no_checker("Do you want to skip "
 if do_show_instructions is True:
     instructions()
 
+# Choose options (numbers or weekdays)
 while True:
     which_quiz = str(input("Do you want to be tested on:\n"
                            "(1): Numbers\n"
                            "(2): Days of the week\n"
                            "Response: "))
     print()
+
     if which_quiz == "1":
         main_questions_stored = number_questions_stored_main
         main_questions_constant = number_questions_constant
         repeat = 10
         break
+
     elif which_quiz == "2":
         main_questions_stored = days_of_the_week_stored_main
         main_questions_constant = days_of_the_week_constant
         repeat = 7
         break
+
     else:
         print('''Please answer either "1" or "2"''')
 
-# if correct_answer == user_input:
-#         return [True, "Nothing here", question_picked]
-#     else:
-#         return [False, question_picked, question_picked]
+# For loop running for the total amount of rounds (10, or 7)
 for item in range(0, repeat):
     print("Round {} out of {}".format(round_number, repeat))
     round_number += 1
-    # print(f"{main_questions_stored}")
     options_output = generate_options(main_questions_stored,
                                       main_questions_constant)
-    # print(f"{options_output} options output")
     question = options_output[1]
     main_questions_stored.remove(question)
     if options_output[0] is False:
@@ -192,20 +198,25 @@ for item in range(0, repeat):
     else:
         right_questions.append(question)
 print("Wrong answers: \n")
-# print(wrong_questions)
 length = len(wrong_questions)
-# print(f"This is the length {length}")
+
+# Prints wrong answers
 for wrong_list_number in range(0, length):
     try:
         wrong_answer = wrong_questions[0]
         print(f"{wrong_answer[1]} in Māori is {wrong_answer[0]}")
         wrong_questions.remove(wrong_answer)
     except IndexError:
-        print("This shouldn't be a problem contact Thomas for bug fixing")
+        print("This shouldn't be a problem as this code is unreachable "
+              "\ncontact Thomas for bug fixing")
         break
+
 print()
+# Prints how many you got correct out of the total rounds eg 1 out of 7
 print(f"You got {round_number - length -1} out of {repeat}")
 print()
+
+# Tells user how many seconds they have played for
 print(f"You played for {(time.time() - game_start):.0f} seconds")
 print("Thanks for playing")
 print("Goodbye")
